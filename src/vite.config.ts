1980302f04f2b59815658266fd668d11ed976ee1
Heyan Zhu <zhu_heyan@163.com>
2024-06-24 17:45:49 +0800

    update vite config

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: "./",
-    publicDir: "assets/"
+    publicDir: "assets/",
+    build: {
+        outDir: "../dist"
+    }
 })
